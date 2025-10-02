f6e3d722b19cdefb34ad447d732ba4d093e0b813
AlexDyatlov <alexdyatlov.web@gmail.com>
2025-10-02 09:29:42 +0300

    Add GitHub Actions workflow for deployment to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  server: {
-    host: true,
-    port: Number(process.env.VITE_PORT) || 8080,
-    hmr: true
-  },
-  preview: {
-    port: Number(process.env.VITE_PORT) || 8080
-  },
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['import']
-      },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/vue-progressbar/' : '/',
+    server: {
+      host: true,
+      port: Number(process.env.VITE_PORT) || 8080,
+      hmr: true
+    },
+    preview: {
+      port: Number(process.env.VITE_PORT) || 8080
+    },
+    plugins: [vue()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['import']
+        }
+      }
     }
-  },
-})
+  };
+});
